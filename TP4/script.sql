b4d7ae21c6627e3ab09eb22fc3e7e543f7df5ceb
Abdessamad Mektoubi <abdessamad.mektoubi@lydec.co.ma>
2022-12-25 12:33:12 +0100

    tp3 et tp4

---
+++ TP4/script.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE public.nature (
+	code serial4 NOT NULL,
+	libelle varchar NULL,
+	CONSTRAINT nature_pk PRIMARY KEY (code)
+);
+
+
+
+CREATE TABLE public."type" (
+	code serial4 NOT NULL,
+	libelle varchar NULL,
+	CONSTRAINT nature_pk_1 PRIMARY KEY (code)
+);
+
+
+
+CREATE TABLE public.client (
+	id int4 NOT NULL DEFAULT nextval('type_1_id_seq'::regclass),
+	prenom varchar NULL,
+	nom varchar NULL,
+	CONSTRAINT type_1_pk PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE public."transaction" (
+	id int4 NOT NULL,
+	cd_type int4 NULL,
+	cd_nature int4 NULL,
+	id_client int4 NULL,
+	"date" timestamp NULL,
+	montant int8 NULL,
+	CONSTRAINT transaction_pk PRIMARY KEY (id),
+	CONSTRAINT transaction_fk FOREIGN KEY (id_client) REFERENCES public.client(id),
+	CONSTRAINT transaction_fk_1 FOREIGN KEY (cd_type) REFERENCES public."type"(code),
+	CONSTRAINT transaction_fk_2 FOREIGN KEY (cd_nature) REFERENCES public.nature(code)
+);
